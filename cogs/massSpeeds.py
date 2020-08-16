d697a7b0d45d66dc5a30fe34ba2f3c3e53e5248f
Ben Roberson <bendroberson@gmail.com>
2020-08-15 21:59:34 -0400

    fixed typo v3

--- cogs/massSpeeds.py
+++ cogs/massSpeeds.py
@@ -11,7 +11,7 @@ class massSpeeds(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def printTeams(self, finalTeams, leftoverList):
+    def printTeams(self, ctx, finalTeams, leftoverList):
         for k,v in finalTeams.items():
             print(k, "is:", v)
         print("Leftovers for this round are:", ', '.join(leftoverList))
